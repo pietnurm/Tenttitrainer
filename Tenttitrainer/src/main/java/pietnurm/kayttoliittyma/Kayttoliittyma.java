0d198f7ed3f4c97ca4f1dcb42e2abe117321922f
Pietari Nurmi <Pietari Nurmi>
2017-04-16 15:41:51 +0300

    kayttoliittymaan CardLayout

--- Tenttitrainer/src/main/java/pietnurm/kayttoliittyma/Kayttoliittyma.java
+++ Tenttitrainer/src/main/java/pietnurm/kayttoliittyma/Kayttoliittyma.java
@@ -5,6 +5,7 @@
  */
 package pietnurm.kayttoliittyma;
 
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -24,6 +25,9 @@ import javax.swing.WindowConstants;
  */
 public class Kayttoliittyma implements Runnable {
     private JFrame frame;
+    private JPanel cards;
+    private final static String VALIKKOPANEL = "valikko";
+    private final static String EDITPANEL = "kysymyseditori";
 
     public Kayttoliittyma() {
     }
@@ -36,31 +40,36 @@ public class Kayttoliittyma implements Runnable {
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        luoKomponentit(frame.getContentPane());
+//        luoKomponentit(frame.getContentPane());
+        luoKomponentit();
 
+        frame.add(cards);
+        
         frame.pack();
         frame.setVisible(true);
     }
 
-    private void luoKomponentit(Container container) {
-        GridLayout layout = new GridLayout(1,1);
-        container.setLayout(layout);
-       
-        container.add(luoValikko());
+    private void luoKomponentit() {
         
-    }
-    private JPanel luoValikko() {
+        // luo paavalikon
         JPanel valikko = new JPanel(new GridLayout(5, 1));
+        valikko.setBackground(new Color(0xffffff));
         
         JLabel otsikko = new JLabel("TENTTITRAINER", SwingConstants.CENTER);
         otsikko.setFont(new Font("Rockwell", Font.PLAIN, 36));
-        valikko.setBackground(new Color(0xffffff));
         valikko.add(otsikko);
         
         JButton luo = new JButton("Luo kysymyksiä");
         luo.setFont(new Font("Rockwell", Font.PLAIN, 20));
         luo.setBackground(new Color(0xffffdd));
-//        luo.addActionListener(nappikuuntelijaKysymyseditori);
+        luo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                CardLayout cl = (CardLayout) (cards.getLayout());
+                cl.show(cards, EDITPANEL);
+            }
+        });
+      
         valikko.add(luo);
         
         JButton testaa = new JButton("Testaa itseäsi");
@@ -73,33 +82,76 @@ public class Kayttoliittyma implements Runnable {
         tulokset.setBackground(new Color(0xffffdd));
         valikko.add(tulokset);
         
-        return valikko;
+        // luo paapaneelin, joka pitaa sisallaan kaikki muut
+        this.cards = new JPanel(new CardLayout());
+        
+        Kysymyseditori editori = new Kysymyseditori();
+        JPanel editPanel = editori.luo();
+        
+        cards.add(valikko, VALIKKOPANEL);
+        cards.add(editPanel, EDITPANEL);
+        
     }
-    private JPanel luoKysymyseditori() {
-        JPanel valikko = new JPanel(new GridLayout(5, 1));
         
-        JLabel otsikko = new JLabel("Luo kysymyksiä", SwingConstants.CENTER);
-        otsikko.setFont(new Font("Rockwell", Font.PLAIN, 36));
-        valikko.setBackground(new Color(0xffffff));
-        valikko.add(otsikko);
         
-        JButton luo = new JButton("Luo kysymyksiä");
-        luo.setFont(new Font("Rockwell", Font.PLAIN, 20));
-        luo.setBackground(new Color(0xffffdd));
-        valikko.add(luo);
-        
-        JButton testaa = new JButton("Testaa itseäsi");
-        testaa.setFont(new Font("Rockwell", Font.PLAIN, 20));
-        testaa.setBackground(new Color(0xffffff));
-        valikko.add(testaa);
-        
-        JButton tulokset = new JButton("Tulokset");
-        tulokset.setFont(new Font("Rockwell", Font.PLAIN, 20));
-        tulokset.setBackground(new Color(0xffffdd));
-        valikko.add(tulokset);
-        
-        return valikko;
-    }
+//    private void luoKomponentit(Container container) {
+//        GridLayout layout = new GridLayout(1,1);
+//        container.setLayout(layout);
+//       
+//        container.add(luoValikko());
+//        
+//    }
+//    private JPanel luoValikko() {
+//        JPanel valikko = new JPanel(new GridLayout(5, 1));
+//        
+//        JLabel otsikko = new JLabel("TENTTITRAINER", SwingConstants.CENTER);
+//        otsikko.setFont(new Font("Rockwell", Font.PLAIN, 36));
+//        valikko.setBackground(new Color(0xffffff));
+//        valikko.add(otsikko);
+//        
+//        JButton luo = new JButton("Luo kysymyksiä");
+//        luo.setFont(new Font("Rockwell", Font.PLAIN, 20));
+//        luo.setBackground(new Color(0xffffdd));
+////        luo.addActionListener(nappikuuntelijaKysymyseditori);
+//        valikko.add(luo);
+//        
+//        JButton testaa = new JButton("Testaa itseäsi");
+//        testaa.setFont(new Font("Rockwell", Font.PLAIN, 20));
+//        testaa.setBackground(new Color(0xffffff));
+//        valikko.add(testaa);
+//        
+//        JButton tulokset = new JButton("Tulokset");
+//        tulokset.setFont(new Font("Rockwell", Font.PLAIN, 20));
+//        tulokset.setBackground(new Color(0xffffdd));
+//        valikko.add(tulokset);
+//        
+//        return valikko;
+//    }
+//    private JPanel luoKysymyseditori() {
+//        JPanel valikko = new JPanel(new GridLayout(5, 1));
+//        
+//        JLabel otsikko = new JLabel("Luo kysymyksiä", SwingConstants.CENTER);
+//        otsikko.setFont(new Font("Rockwell", Font.PLAIN, 36));
+//        valikko.setBackground(new Color(0xffffff));
+//        valikko.add(otsikko);
+//        
+//        JButton luo = new JButton("Luo kysymyksiä");
+//        luo.setFont(new Font("Rockwell", Font.PLAIN, 20));
+//        luo.setBackground(new Color(0xffffdd));
+//        valikko.add(luo);
+//        
+//        JButton testaa = new JButton("Testaa itseäsi");
+//        testaa.setFont(new Font("Rockwell", Font.PLAIN, 20));
+//        testaa.setBackground(new Color(0xffffff));
+//        valikko.add(testaa);
+//        
+//        JButton tulokset = new JButton("Tulokset");
+//        tulokset.setFont(new Font("Rockwell", Font.PLAIN, 20));
+//        tulokset.setBackground(new Color(0xffffdd));
+//        valikko.add(tulokset);
+//        
+//        return valikko;
+//    }
     
 
     public JFrame getFrame() {
--- Tenttitrainer/src/main/java/pietnurm/kayttoliittyma/NappikuuntelijaKysymyseditori.java
+++ Tenttitrainer/src/main/java/pietnurm/kayttoliittyma/NappikuuntelijaKysymyseditori.java
@@ -1,28 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pietnurm.kayttoliittyma;
-
-import java.awt.Container;
-import java.awt.event.ContainerListener;
-
-/**
- *
- * @author pieta
- */
-public class NappikuuntelijaKysymyseditori implements ContainerListener {
-    
-    private Container container;
-    
-    public void NappikuuntelijaKysymyseditori (Container container) {
-        this.container = container;
-    }
-    
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        container.luoKysymyseditori();
-    }
-    
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package pietnurm.kayttoliittyma;
+//
+//import java.awt.Container;
+//import java.awt.event.ContainerListener;
+//
+///**
+// *
+// * @author pieta
+// */
+//public class NappikuuntelijaKysymyseditori implements ContainerListener {
+//    
+//    private Container container;
+//    
+//    public void NappikuuntelijaKysymyseditori (Container container) {
+//        this.container = container;
+//    }
+//    
+//    @Override
+//    public void actionPerformed(ActionEvent ae) {
+//        container.luoKysymyseditori();
+//    }
+//    
+//}
